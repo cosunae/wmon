83e7451dc1583215eb759bdecbde474444544d82
cosunae <carlos.osuna@meteoswiss.ch>
2021-09-06 22:48:35 +0200

    add missing interface

--- wmon_c.h
+++ wmon_c.h
@@ -4,6 +4,7 @@
 extern "C" {
     wmon* wmon_init();
     void wmon_push_metric(const wmon* wm, const char* measurement, const char* fieldvalpair, size_t timestamp);
+    void wmon_push_metric_tag(const wmon* wm, const char* measurement, const char* fieldvalpair, size_t timestamp, const char* tag);
     void wmon_destroy(wmon* wm);
     size_t timestamp();
 }
